5d58ef060d02538a3ef1a8be09d12fa6927c7a53
Mariel <marielvaca@gmail.com>
2022-06-29 17:45:53 -0700

    added token cookie route

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-//test route
-router.get('/hello/world', function(req, res) {
-  res.cookie('XSRF-TOKEN', req.csrfToken());
-  res.send('Hello World!');
-});
+//allows any developer to re-set CSRF token cookie
+router.get("/api/csrf/restore", (req, res) => {
+    const csrfToken = req.csrfToken();
+    res.cookie("XSRF-TOKEN", csrfToken);
+    res.status(200).json({
+      'XSRF-Token': csrfToken
+    });
+  });
 
 module.exports = router;
