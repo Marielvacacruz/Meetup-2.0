fb235f33b8bc54425a58398e632c9992023a7946
Mariel <marielvaca@gmail.com>
2022-07-02 12:14:24 -0700

    Added error-handling middleware for common errors and debugging

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,7 @@ const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 
 const routes = require('./routes');
+const { ValidationError } = require('sequelize');
 
 const { environment } = require('./config');
 const isProduction = environment ===  'production';
@@ -47,6 +48,42 @@ if (!isProduction) {
   );
 
   app.use(routes);
+
+  //Error Handling Middlewares
+
+  //catch unhandled requests and forward to error handler
+  app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = ["The Requested resource couldn't be found."];
+    err.status = 404;
+    next(err);
+  });
+
+  //Process sequelize errors
+  app.use((err, _req, _res, next) => {
+    // check if error is a Sequelize error:
+    if (err instanceof ValidationError) {
+      err.errors = err.errors.map((e) => e.message);
+      err.title = 'Validation error';
+    }
+    next(err);
+  });
+
+  //Error formatter
+  app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    const options = {};
+    if (!isProduction) options.stack = err.stack;
+    res.json({
+      message: err.message,
+      statusCode: res.statusCode,
+      errors: err.errors,
+      ...options,
+    });
+  });
+
 
 
   module.exports = app;
