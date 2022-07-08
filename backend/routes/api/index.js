0ae62ff340cdebfe765c2a8c9aac9e02fe8675d3
Mariel <marielvaca@gmail.com>
2022-07-07 20:35:17 -0500

    added express validator & login/logout routes

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,4 +1,10 @@
 const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users');
+
+router.use('/session', sessionRouter);
+router.use('/users', usersRouter);
+
 //test route
 router.post('/test', function(req, res) {
     res.json({requestBody: req.body});
---
+++ backend/routes/api/session.js
@@ -0,0 +1,53 @@
+const express = require('express');
+
+const { setTokenCookie, restoreUser } = require('../../utils/auth');
+const { User } = require('../../db/models');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
+const router = express.Router();
+
+//validate login
+const validateLogin = [
+    check('credential')
+        .exists({ checkFalsy: true })
+        .notEmpty()
+        .withMessage('Please provide a valid email.'),
+    check('password')
+        .exists({ checkFalsy: true })
+        .withMessage('Pleaser provide a password.'),
+    handleValidationErrors
+];
+
+//Log in
+router.post('/', validateLogin, async (req, res,  next) => {
+    const { credential, password } = req.body;
+
+    const user = await User.login({ credential, password});
+
+    if (!user) {
+        const err = new Error('Invalid credentials');
+        err.status = 401;
+        err.title = 'Login Failed';
+        err.errors = ['The provided credentials were invalid. '];
+        return next(err);
+    }
+
+    await setTokenCookie(res, user);
+    const { token } = req.cookies;
+
+    return res.json({
+        ...user.toSafeObject(),
+        token
+    });
+});
+
+//Log out
+
+router.delete('/', (_req, res) => {
+    res.clearCookie('token');
+    return res.json({ message: 'success' });
+});
+
+
+module.exports = router;
