0d6b8e70769a986a14c5e6fb6fa390eee23edd80
Mariel <marielvaca@gmail.com>
2022-07-16 11:57:50 -0500

    edit a group route

--- backend/routes/api/groups.js
+++ backend/routes/api/groups.js
@@ -66,16 +66,6 @@ router.get("/:groupId", async (req, res) => {
     },
   });
 
-  // const image = await Image.findAll({
-  //   where: {
-  //   imageableId: groupId,
-  //   imageableType: 'Group'
-  //   },
-  //   attributes: {
-  //     exclude: ['createdAt', 'updatedAt', 'imageableType']
-  //   }
-  // });
-
   if (Groups.id === null) {
     let err = new Error("Group Could not be found");
     err.status = 404;
@@ -122,6 +112,44 @@ router.post("/", requireAuth, validateGroup, async (req, res) => {
   });
 
   return res.json(newGroup);
+});
+
+//Edit a Group
+
+router.put("/:groupId", requireAuth, validateGroup, async (req, res) => {
+  const { user } = req;
+  let { groupId } = req.params;
+  groupId = parseInt(groupId);
+  const { name, about, type, private, city, state } = req.body;
+
+  const group = await Group.findByPk(groupId);
+
+  if (!group) {
+    res.status(404);
+    return res.json({
+      message: "Group could not be found",
+      statusCode: 404,
+    });
+  }
+
+  if (user.id !== group.organizerId) {
+    res.status(403);
+    return res.json({
+      message: "Forbidden",
+      statusCode: 403,
+    });
+  }
+
+  await group.update({
+    name,
+    about,
+    type,
+    private,
+    city,
+    state,
+  });
+
+  res.json(group);
 });
 
 
