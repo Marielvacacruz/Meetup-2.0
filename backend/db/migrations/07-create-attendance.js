42a76d12a76e28b3ba12e149074b9b9b44cfbd8f
Mariel <marielvaca@gmail.com>
2022-07-19 18:33:12 -0500

    added attendance table and model

---
+++ backend/db/migrations/07-create-attendance.js
@@ -0,0 +1,46 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Attendances', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        allowNull: false,
+        references: {
+          model: 'Users',
+          key: 'id'
+        },
+        type: Sequelize.INTEGER
+      },
+      eventId: {
+        allowNull: false,
+        references: {
+          model: 'Events',
+          key: 'id'
+        },
+        type: Sequelize.INTEGER
+      },
+      status: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Attendances');
+  }
+};
---
+++ backend/db/models/attendance.js
@@ -0,0 +1,46 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Attendance extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Attendance.belongsTo(
+        models.Event, {foreignKey: 'eventId'}
+      );
+
+      Attendance.belongsTo(
+        models.User, {foreignKey: 'userId'}
+      );
+    }
+  }
+  Attendance.init({
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    userId:  {
+      allowNull: false,
+      type: DataTypes.INTEGER
+    },
+    eventId:{
+      allowNull: false,
+      type: DataTypes.INTEGER
+    },
+    status: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+  }, {
+    sequelize,
+    modelName: 'Attendance',
+  });
+  return Attendance;
+};
