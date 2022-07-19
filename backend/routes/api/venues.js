90d592715c8fc4c683324e41a0ed6b4b9f0e71cb
Mariel <marielvaca@gmail.com>
2022-07-19 16:04:17 -0500

    delete a venue route added

--- backend/routes/api/venues.js
+++ backend/routes/api/venues.js
@@ -65,4 +65,54 @@ router.put("/:venueId", requireAuth, validateVenue, async (req, res, next) => {
     }
   });
 
+//Delete a Venue
+router.delete("/:venueId", requireAuth, async (req, res) => {
+    const { user } = req;
+    const { venueId } = req.params;
+
+    const venue = await Venue.findByPk(venueId);
+
+    if (!venue) {
+        res.status(404);
+        return res.json({
+            message: "Venue couldn't be found",
+            statusCode: 404
+        });
+    };
+
+    const group = venue.groupId;
+
+    const owner = await Membership.findOne({
+        where: {
+          memberId: user.id,
+          groupId: group
+        }
+      });
+
+      if (!owner) {
+        const err = new Error("Membership not found");
+        err.message = "Membership not found";
+        err.status = 404;
+        return next(err);
+      }
+
+      if (owner.status === "host" || owner.status === "co-host") {
+        await venue.destroy();
+        return res.json({
+            message: "Successfully Deleted Venue",
+            statusCode: 200,
+          });
+
+      }
+      else {
+          res.status(403);
+          return res.json({
+              message: "Forbidden",
+              statusCode: 403
+          });
+      }
+
+});
+
+
 module.exports = router;
